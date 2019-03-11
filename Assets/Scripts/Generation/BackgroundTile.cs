c92118df14829f5bdf2d7f2a4ed1742cd72299d0
jakemakesgames <jakebrown.designer@gmail.com>
2019-03-11 17:23:50 +1100

    pushed meta files

---
+++ Assets/Scripts/Generation/BackgroundTile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+    public GameObject[] tiles;
+
+    
+    void Start()
+    {
+        // Call the Initialize function
+        Initialize();
+    }
+
+    void Initialize()
+    {
+        // choose a random number between 0 and the amount of tiles in the array
+        int tileToUse = Random.Range(0, tiles.Length);
+        // Instantiate a random tile from the array (based off the tileToUse result) at this tiles position with no rotation
+        GameObject tile = Instantiate(tiles[tileToUse], transform.position, Quaternion.identity) as GameObject;
+        // Set the Parent object of the tile to the BackgroundTile GameObject
+        tile.transform.parent = this.transform;
+        // Set the name of each background tile equal to their width and height position on the grid
+        tile.name = this.gameObject.name;
+    }
+}
---
+++ Assets/Scripts/Generation/Board.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles;
+
+    void Start()
+    {
+        // Tell the allTiles arrway how big it needs to be 
+        allTiles = new BackgroundTile[width, height];
+        // Call the SetUp function
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        // Cycle through the Width and Height and Instantiate a Tile Prefab at
+        // every position
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                // Create a new temporary variable - a Vector2 for the W & H pos
+                Vector2 tempPosition = new Vector2(i, j);
+                // Instantiate the tilePrefab as a GameObject
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                // Set the Parent object of the backgroundTile to the Board GameObject
+                backgroundTile.transform.parent = this.transform;
+                // Set the name of each background tile equal to their width and height position on the grid
+                backgroundTile.name = "( " + i + " , " + j + " )";
+
+            }
+        }
+    }
+}
