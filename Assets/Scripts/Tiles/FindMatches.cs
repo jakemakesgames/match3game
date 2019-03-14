7b87d2afd141578eb8b3bb65bbc777d7c9fcd7d2
jakemakesgames <jakebrown.designer@gmail.com>
2019-03-15 00:10:01 +1100

    column + row bombs implemented - tested via debug function

--- Assets/Scripts/Tiles/FindMatches.cs
+++ Assets/Scripts/Tiles/FindMatches.cs
@@ -37,15 +37,51 @@ public class FindMatches : MonoBehaviour
                         GameObject rightTile = board.allShapeTiles[i + 1, j];
                         if (leftTile != null && rightTile != null)
                         {
+                            // If the left and right tags are equal to the current tiles tag
                             if (leftTile.tag == currentTile.tag && rightTile.tag == currentTile.tag)
                             {
+                                // If either the left, right or current tiles are row bombs
                                 if (currentTile.GetComponent<ShapeTile>().isRowBomb || leftTile.GetComponent<ShapeTile>().isRowBomb || rightTile.GetComponent<ShapeTile>().isRowBomb)
                                 {
+                                    // Do the thing
                                     currentMatches.Union(GetRowPieces(j));
                                 }
 
+                                // Check to see if the current, left or right tiles are also column bombs
+                                if (currentTile.GetComponent<ShapeTile>().isColumnBomb)
+                                {
+                                    currentMatches.Union(GetColumnPieces(i));
+                                }
+
+                                // Check to see if the left edge tile is a column bomb
+                                if (leftTile.GetComponent<ShapeTile>().isColumnBomb)
+                                {
+                                    currentMatches.Union(GetColumnPieces(i - 1));
+                                }
+
+                                // Check to see if the right edge tile is a column bomb
+                                if (rightTile.GetComponent<ShapeTile>().isColumnBomb)
+                                {
+                                    currentMatches.Union(GetColumnPieces(i + 1));
+                                }
+
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(leftTile))
+                                {
+                                    currentMatches.Add(leftTile);
+                                }
                                 leftTile.GetComponent<ShapeTile>().isMatched = true;
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(rightTile))
+                                {
+                                    currentMatches.Add(rightTile);
+                                }
                                 rightTile.GetComponent<ShapeTile>().isMatched = true;
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(currentTile))
+                                {
+                                    currentMatches.Add(currentTile);
+                                }
                                 currentTile.GetComponent<ShapeTile>().isMatched = true;
                             }
                         }
@@ -58,8 +94,48 @@ public class FindMatches : MonoBehaviour
                         {
                             if (upTile.tag == currentTile.tag && downTile.tag == currentTile.tag)
                             {
+                                // If either the left, right or current tiles are column bombs
+                                if (currentTile.GetComponent<ShapeTile>().isColumnBomb || upTile.GetComponent<ShapeTile>().isColumnBomb || downTile.GetComponent<ShapeTile>().isColumnBomb)
+                                {
+                                    // Do the thing
+                                    currentMatches.Union(GetColumnPieces(i));
+                                }
+
+                                // Check to see if the middle piece is a row bomb
+                                if (currentTile.GetComponent<ShapeTile>().isRowBomb)
+                                {
+                                    currentMatches.Union(GetRowPieces(j));
+                                }
+
+                                // Check to see if the up piece is a row bomb
+                                if (upTile.GetComponent<ShapeTile>().isRowBomb)
+                                {
+                                    currentMatches.Union(GetRowPieces(j + 1));
+                                }
+
+                                // Check to see if the down piece is a row bomb
+                                if (downTile.GetComponent<ShapeTile>().isRowBomb)
+                                {
+                                    currentMatches.Union(GetRowPieces(j - 1));
+                                }
+
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(upTile))
+                                {
+                                    currentMatches.Add(upTile);
+                                }
                                 upTile.GetComponent<ShapeTile>().isMatched = true;
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(downTile))
+                                {
+                                    currentMatches.Add(downTile);
+                                }
                                 downTile.GetComponent<ShapeTile>().isMatched = true;
+                                // ADD THE TILE TO THE LIST IF IT'S NOT ALREADY IN IT
+                                if (!currentMatches.Contains(currentTile))
+                                {
+                                    currentMatches.Add(currentTile);
+                                }
                                 currentTile.GetComponent<ShapeTile>().isMatched = true;
                             }
                         }
