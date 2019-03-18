20e004c7f2b51aa0bbdfe894fd227ed5a689b97d
jakemakesgames <jakebrown.designer@gmail.com>
2019-03-18 20:42:51 +1100

    created a little animated menu to play before a level to tell the player what theor goals are for that level

---
+++ Assets/Scripts/Managers/AnimationManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Animator fadePanelAnim;
+    public Animator goalPanelAnim;
+
+    public void OK()
+    {
+        if (fadePanelAnim != null && goalPanelAnim != null)
+        {
+            // Trigger both "Out" bools to true
+            fadePanelAnim.SetBool("Out", true);
+            goalPanelAnim.SetBool("Out", true);
+        }
+    }
+}
