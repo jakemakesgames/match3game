21ff7676ca2dee2df9b5b9e4ebdecb90f93c8aa5
jakemakesgames <jakebrown.designer@gmail.com>
2019-03-18 23:00:59 +1100

    started working on the foundations of the goal manager and got it hooked up to the UI panels on a basic level

---
+++ Assets/Scripts/Managers/GoalManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Data Container
+[System.Serializable]
+public class BlankGoal
+{
+    public int numberNeeded;
+    public int numberCollected;
+    public Sprite goalSprite;
+    public string matchValue;
+}
+
+public class GoalManager : MonoBehaviour
+{
+    public BlankGoal[] levelGoals;
+    public GameObject goalPrefab;
+    public GameObject goalIntroParent;
+    public GameObject goalGameParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetupIntroGoals();
+    }
+
+    void SetupIntroGoals()
+    {
+        // Loop through all of the level goals in the array
+        for (int i = 0; i < levelGoals.Length; i++)
+        {
+            // Create a new Goal panel at the goal intro parent position
+            GameObject goal = Instantiate(goalPrefab, goalIntroParent.transform.position, Quaternion.identity);
+            goal.transform.SetParent(goalIntroParent.transform);
+            // Set the image and text of the goal:
+            GoalPanel panel = goal.GetComponent<GoalPanel>();
+            panel.thisSprite = levelGoals[i].goalSprite;
+            panel.thisString = "0/ " + levelGoals[i].numberNeeded;
+
+            // Create the in game goals
+            GameObject gameGoal = Instantiate(goalPrefab, goalGameParent.transform.position, Quaternion.identity);
+            gameGoal.transform.SetParent(goalGameParent.transform);
+            panel = gameGoal.GetComponent<GoalPanel>();
+            panel.thisSprite = levelGoals[i].goalSprite;
+            panel.thisString = "0/ " + levelGoals[i].numberNeeded;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
